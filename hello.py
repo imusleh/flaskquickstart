c1587168484451f7d47df6997aae2d4163b9cbec
Islam Musleh <islam.musleh@gmail.com>
2018-11-05 18:16:36 -0800

    Update added notes,welcome pg , style

--- hello.py
+++ hello.py
@@ -1,25 +1,10 @@
-from flask import Flask
+# import the Flask class from the flask module
+from flask import Flask, render_template
+
+# create the application object
 app = Flask(__name__)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    #show the user profile for that user
-    return 'imusleh %s' % username
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % subpath
-
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
