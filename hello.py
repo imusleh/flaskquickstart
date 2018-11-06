ed701993a7e7cb2acc1fa72b1090549c049c1633
Islam Musleh <islam.musleh@gmail.com>
2018-11-05 18:26:56 -0800

    mod

--- hello.py
+++ hello.py
@@ -4,6 +4,13 @@ from flask import Flask, render_template
 # create the application object
 app = Flask(__name__)
 
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+#generate URLs for static files
+url_for('static', filename='style.css')
 
 @app.route('/welcome')
 def welcome():
